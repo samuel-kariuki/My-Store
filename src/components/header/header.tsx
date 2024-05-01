1094e21fa4811b348e9e62b87e7619c4e36fd6fa
unknown <samuelkariuki942@gmail.com>
2024-05-01 05:48:49 +0200

    updated pages

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,34 +3,53 @@ import Link from "next/link";
 import { SearchInput } from "../search-input/search-input";
 import styles from "./header.module.scss";
 import { Cart } from "../cart/cart";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles["header-container"]}>
-      <Link href={"/"} className={styles["title"]}>
-        Sam-store
-      </Link>
-
-        {/* Navigation Tabs */}
-        <nav className={styles["nav"]}>
-
-        <Link href={"/"} className={styles["nav-link"]}>
-        Home
-      </Link>
-
-      <Link href={"/category"} className={styles["nav-link"]}>
-        Category
-      </Link>
-        
-      <Link href={"/contact"} className={styles["nav-link"]}>
-        Contact
-      </Link>
-
-      </nav>
-
+      <div className={styles["header-left"]}>
+        <Link href={"/"} className={styles["title"]}>
+          Sam-store
+        </Link>
+        <nav
+          className={`${styles["nav"]} ${isMenuOpen ? styles["nav--open"] : ""}`}
+        >
+          <div className={styles["nav__links"]}>
+            <Link href={"/"} className={styles["nav-link"]}>
+              Home
+            </Link>
+            <Link href={"/category"} className={styles["nav-link"]}>
+              Category
+            </Link>
+            <Link href={"/ContactForm"} className={styles["nav-link"]}>
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
       <div className={styles["top-right"]}>
         <Cart />
-        <SearchInput />
+        <div className={styles["search-toggle"]}>
+          <button
+            className={styles["nav-toggle"]}
+            onClick={toggleMenu}
+            aria-label="Toggle navigation"
+          >
+            {isMenuOpen ? (
+              <span className={styles["nav-toggle__close"]}>&times;</span>
+            ) : (
+              <span className={styles["nav-toggle__hamburger"]}>&#9776;</span>
+            )}
+          </button>
+          <SearchInput className={styles["search-input"]} />
+        </div>
       </div>
     </header>
   );
